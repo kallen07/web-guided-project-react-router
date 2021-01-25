f9148f4c17e550fe87cffe0c0f9c69ceaf611ada
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-25 11:54:50 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -38,7 +38,7 @@ export default function App(props) {
         <Home />
       </Route>
       <Route path="/items-list">
-        <ItemsList items={[]} />
+        <ItemsList items={stock} />
       </Route>
     </div>
   );
