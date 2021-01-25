80b9e5ee0bc65743db4d067c355b8869028a68c1
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-25 11:40:44 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,12 @@ export default function App(props) {
         <h1 className="store-header">Emily&apos;s Trinkets</h1>
         <div className="nav-links">
           {/* 👉 STEP 3 - Make Links to navigate us Home (`/`) and Shop (`/items-list`) */}
-          <a href="/">Home</a>
+          <a 
+          onClick={(evt) => {
+            evt.preventDefault()
+            history
+          }}
+          href="/">Home</a>
         </div>
       </nav>
 
