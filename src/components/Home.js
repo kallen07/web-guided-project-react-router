d913095ea07d63659f5cbbef5ef54be416b94e0e
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-25 12:11:04 -0600

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ export default function Home() {
   const history = useHistory();
 
   const routeToShop = () => {
+    // react router own 
     console.log(history);
     history.push("/items-list");
   };
